de011707822352b2cf62cc1f99fee392ff2a71e3
danielmarbach <daniel.marbach@openplace.net>
2015-11-23 22:22:36 +0100

    Autosetup database

--- SetupDatabases.sql
+++ SetupDatabases.sql
@@ -1,11 +1,6 @@
 USE [master]
 GO
 
-/****** Object:  Database [Sales]    Script Date: 2015-11-20 13:24:16 ******/
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Sales')
 CREATE DATABASE [Sales]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'Sales', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\Sales.mdf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
- LOG ON 
-( NAME = N'Sales_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\Sales_log.ldf' , SIZE = 2048KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
 GO
