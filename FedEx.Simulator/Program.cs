526cacd20721553718c7c96c653426ff706b0e66
danielmarbach <daniel.marbach@particular.net>
2015-11-24 17:46:20 +0100

    Simple FedEx simulator

---
+++ FedEx.Simulator/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FedEx.Simulator
+{
+    using System;
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var behaviors = new FedexBehavior[]
+            {
+                new Success(),
+                new ThrowTimeoutException(),
+                new Success(),
+                new TakeLonger(),  
+            };
+
+            BehaviorHolder.Behavior = behaviors[0];
+
+            using (var host = new NancyHost(new Uri("http://localhost:8888")))
+            {
+                host.Start();
+                Console.WriteLine("What behavior would you like?");
+                Console.WriteLine("[1] TimeoutException");
+                Console.WriteLine("[2] Success (default)");
+                Console.WriteLine("[3] Take longer than logical timeout");
+                Console.WriteLine();
+                Console.WriteLine("Please press 'q' to exit.");
+
+                string key;
+                do
+                {
+                    key = Console.ReadKey().KeyChar.ToString().ToLowerInvariant();
+                    int index;
+                    if (int.TryParse(key, out index))
+                    {
+                        BehaviorHolder.Behavior = behaviors[index];
+                    }
+                } while (key != "q");
+            }
+        }
+    }
+}
---
+++ FedEx.Simulator/TakeLonger.cs
@@ -0,0 +1,12 @@
+﻿namespace FedEx.Simulator
+{
+    using System.Threading;
+
+    public class TakeLonger : FedexBehavior
+    {
+        public void Simulate()
+        {
+            Thread.Sleep(15000);
+        }
+    }
+}
