9b66158721c36522085cb16138abce7db36cea82
danielmarbach <daniel.marbach@particular.net>
2015-11-24 16:52:16 +0100

    Creating shipping database

--- SetupDatabases.sql
+++ SetupDatabases.sql
@@ -4,3 +4,7 @@ GO
 IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Sales')
 CREATE DATABASE [Sales]
 GO
+
+IF  NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'Shipping')
+CREATE DATABASE [Shipping]
+GO
